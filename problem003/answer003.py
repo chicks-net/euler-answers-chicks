bc74131c2e4a6068398e30391f9df12ef0565e80
Christopher Hicks <chicks.net@gmail.com>
2014-07-26 00:21:11 -0700

    slow answer 3 in python

---
+++ problem003/answer003.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+def factor(n):
+	factors = [ ]
+	x = 2
+	while x < n:
+		if n % x == 0:
+			factors.append(x)
+		if (x % 10000000) == 0:
+			print "\tchecking " + str(x) + " for " + str(n) + " (" + str(float(x/n)) + "%)"
+		x=x+1
+
+	print str(n) + " has " + str(len(factors)) + " factors"
+
+	return factors
+
+def prime_factor(n):
+	any_factors = factor(n)
+	prime_factors = []
+	for x in any_factors:
+		x_factors = factor(x)
+		if len(x_factors) == 0:
+			prime_factors.append(x)
+
+	return prime_factors
+
+print "prime factors of 13195 are " + str(prime_factor(13195))
+print "prime factors of 131950 are " + str(prime_factor(131950))
+print "prime factors of 600851475143 are " + str(prime_factor(600851475143))
