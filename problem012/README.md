cf2d251162682dd6be75671cb3b053ea3cd53492
Christopher Hicks <chicks.net@gmail.com>
2014-08-05 07:34:41 -0700

    answer 12 in python (naively slow)

---
+++ problem012/README.md
@@ -0,0 +1,14 @@
+Euler Problem 12
+================
+
+* [Problem definition](http://projecteuler.net/problem=12)
+
+Performance Analysis
+--------------------
+
+The naive implementation takes almost 8 hours:
+
+	real	476m43.205s
+	user	476m47.021s
+	sys	0m3.277s
+
---
+++ problem012/answer012.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+def factor(n):
+	factors = [ 1 ]
+	x = 2
+	while x <= n:
+		if n % x == 0:
+			factors.append(x)
+		if (x % 10000000) == 0:
+			print "\tchecking " + str(x) + " for " + str(n) + " (" + str(float(x)/n) + "%)"
+		x=x+1
+
+#	print str(n) + " has " + str(len(factors)) + " factors"
+
+	return len(factors)
+
+max_divisors = 0
+n = 0
+triangle = 0
+
+while max_divisors < 502:
+	n += 1
+	loop_n = n
+	triangle = 0
+	while loop_n:
+		triangle += loop_n
+		loop_n -= 1
+	divisors = factor(triangle)
+
+	if ( divisors > max_divisors ):
+		max_divisors = divisors
+		print "n=" + str(n) + " triangle " + str(triangle) + " has " + str(max_divisors) + " divisors"
+		
+
+print "ANSWER:" + str(triangle)
