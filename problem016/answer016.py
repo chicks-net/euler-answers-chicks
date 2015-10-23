bb0a4aeab44c6712971cc588ba7ba42f58e8f418
Christopher Hicks <chicks.net@gmail.com>
2015-10-23 10:54:57 -0700

    answer 16 in python :blossom:

---
+++ problem016/answer016.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""chicks' answer to Euler Project problem #56"""
+
+import math
+
+max_factor = 100
+
+def calc_digit_sum(n):
+	digits = list(str(n))
+	sum = 0
+	for digit in digits:
+		sum += int(digit)
+
+	#print str(n) + " -> " + str(sum)
+
+	return sum
+
+digit_sum = calc_digit_sum( 2 ** 1000 )
+
+print "ANSWER:" + str(digit_sum)
