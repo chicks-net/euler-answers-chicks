baf3b1572251f167e5899e4c2005e0f2727d9565
Christopher Hicks <chicks@uber.com>
2015-11-28 09:51:34 -0800

    more spaces and one less space for `pylint` :scissors:

--- problem018/answer018.py
+++ problem018/answer018.py
@@ -11,7 +11,7 @@ argcount = len(sys.argv)
 if argcount <= 1:
 	raise ValueError("missing filename on command line")
 triangle_filename = sys.argv[1]
-print str(argcount) + " arguments: " + triangle_filename 
+print str(argcount) + " arguments: " + triangle_filename
 
 # read file
 with open(triangle_filename, "r") as triangle_file:
@@ -21,23 +21,23 @@ with open(triangle_filename, "r") as triangle_file:
 triangle = []
 for y in lines:
 	y = string.strip(y)
-	fields = re.split('\s+', y)
+	fields = re.split(r'\s+', y)
 	int_fields = []
 	for f in fields:
 		int_fields.append(int(f))
-	triangle.insert(0,int_fields)
+	triangle.insert(0, int_fields)
 	c = len(fields)
 
-pp = pprint.PrettyPrinter(indent=4)
-#pp.pprint(triangle)
+pretty = pprint.PrettyPrinter(indent=4)
+#pretty.pprint(triangle)
 
 rows = len(triangle) - 1
-for z in range(0,rows):
+for z in range(0, rows):
 	row = triangle[z]
-	pp.pprint(row)
+	pretty.pprint(row)
 
 	entries = len(row) - 1
-	for y in range(0,entries):
+	for y in range(0, entries):
 		a = row[y]
 		b = row[y + 1]
 		#print "comparing " + str(a) + " and " + str(b)
